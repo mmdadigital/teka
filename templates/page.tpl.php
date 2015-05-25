669f66b7f42d9fedc2079c8138b61d18fd0ef890
filipemerker <filipesmerker@gmail.com>
2015-05-25 15:31:37 -0300

    Utilização dos core templates no theme Teka

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,26 +1,98 @@
-<div id="page" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<?php
 
-  <!-- ______________________ HEADER _______________________ -->
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.tpl.php template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup themeable
+ */
+?>
 
-  <header id="header">
-    <div class="container">
+  <div id="page-wrapper"><div id="page">
+
+    <div id="header"><div class="section clearfix">
+
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
         <div id="name-and-slogan">
-
           <?php if ($site_name): ?>
             <?php if ($title): ?>
-              <div id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-              </div>
+              <div id="site-name"><strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </strong></div>
             <?php else: /* Use h1 when the content title is empty */ ?>
               <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
               </h1>
             <?php endif; ?>
           <?php endif; ?>
@@ -28,94 +100,57 @@
           <?php if ($site_slogan): ?>
             <div id="site-slogan"><?php print $site_slogan; ?></div>
           <?php endif; ?>
-
-        </div>
+        </div> <!-- /#name-and-slogan -->
       <?php endif; ?>
 
-      <?php if ($page['header']): ?>
-        <div id="header-region">
-          <?php print render($page['header']); ?>
-        </div>
-      <?php endif; ?>
-    </div>
-  </header> <!-- /header -->
+      <?php print render($page['header']); ?>
 
-  <?php if ($main_menu || $secondary_menu): ?>
-    <nav id="navigation" class="menu <?php if (!empty($main_menu)) {print "with-primary";}
-      if (!empty($secondary_menu)) {print " with-secondary";} ?>">
-      <div class="container">
-        <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu')))); ?>
-        <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary', 'class' => array('links', 'clearfix', 'sub-menu')))); ?>
-      </div>
-    </nav> <!-- /navigation -->
-  <?php endif; ?>
+    </div></div> <!-- /.section, /#header -->
 
-  <!-- ______________________ MAIN _______________________ -->
+    <?php if ($main_menu || $secondary_menu): ?>
+      <div id="navigation"><div class="section">
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+      </div></div> <!-- /.section, /#navigation -->
+    <?php endif; ?>
 
-  <div id="main">
-    <div class="container">
-      <section id="content">
-  
-          <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
-            <div id="content-header">
+    <?php if ($breadcrumb): ?>
+      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+    <?php endif; ?>
 
-              <?php print $breadcrumb; ?>
+    <?php print $messages; ?>
 
-              <?php if ($page['highlighted']): ?>
-                <div id="highlighted"><?php print render($page['highlighted']) ?></div>
-              <?php endif; ?>
+    <div id="main-wrapper"><div id="main" class="clearfix">
 
-              <?php print render($title_prefix); ?>
-
-              <?php if ($title): ?>
-                <h1 class="title"><?php print $title; ?></h1>
-              <?php endif; ?>
-
-              <?php print render($title_suffix); ?>
-              <?php print $messages; ?>
-              <?php print render($page['help']); ?>
-
-              <?php if ($tabs): ?>
-                <div class="tabs"><?php print render($tabs); ?></div>
-              <?php endif; ?>
-
-              <?php if ($action_links): ?>
-                <ul class="action-links"><?php print render($action_links); ?></ul>
-              <?php endif; ?>
-
-            </div> <!-- /#content-header -->
-          <?php endif; ?>
-
-          <div id="content-area">
-            <?php print render($page['content']) ?>
-          </div>
-
-          <?php print $feed_icons; ?>
-
-      </section> <!-- /content-inner /content -->
+      <div id="content" class="column"><div class="section">
+        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div></div> <!-- /.section, /#content -->
 
       <?php if ($page['sidebar_first']): ?>
-        <aside id="sidebar-first" class="column sidebar first">
+        <div id="sidebar-first" class="column sidebar"><div class="section">
           <?php print render($page['sidebar_first']); ?>
-        </aside>
-      <?php endif; ?> <!-- /sidebar-first -->
+        </div></div> <!-- /.section, /#sidebar-first -->
+      <?php endif; ?>
 
       <?php if ($page['sidebar_second']): ?>
-        <aside id="sidebar-second" class="column sidebar second">
+        <div id="sidebar-second" class="column sidebar"><div class="section">
           <?php print render($page['sidebar_second']); ?>
-        </aside>
-      <?php endif; ?> <!-- /sidebar-second -->
-    </div>
-  </div> <!-- /main -->
+        </div></div> <!-- /.section, /#sidebar-second -->
+      <?php endif; ?>
 
-  <!-- ______________________ FOOTER _______________________ -->
+    </div></div> <!-- /#main, /#main-wrapper -->
 
-  <?php if ($page['footer']): ?>
-    <footer id="footer">
-      <div class="container">
+    <div id="footer"><div class="section">
       <?php print render($page['footer']); ?>
-      </div>
-    </footer> <!-- /footer -->
-  <?php endif; ?>
+    </div></div> <!-- /.section, /#footer -->
 
-</div> <!-- /page -->
+  </div></div> <!-- /#page, /#page-wrapper -->
